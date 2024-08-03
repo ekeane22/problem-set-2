22a0a0ba0ec40cace6a78821e4b482b06a0e1ea4
ekeane22 <ekeane@terpmail.umd.edu>
2024-08-03 19:47:08 -0400

    part2

--- src/part1_etl.py
+++ src/part1_etl.py
@@ -4,7 +4,7 @@ PART 1: ETL the two datasets and save each in `data/` as .csv's
 
 import pandas as pd
 from pathlib import Path
-#import requests  
+
 
 pred_universe_raw = pd.read_csv('https://www.dropbox.com/scl/fi/69syqjo6pfrt9123rubio/universe_lab6.feather?rlkey=h2gt4o6z9r5649wo6h6ud6dce&dl=1')
 arrest_events_raw = pd.read_csv('https://www.dropbox.com/scl/fi/wv9kthwbj4ahzli3edrd7/arrest_events_lab6.feather?rlkey=mhxozpazqjgmo6qqahc2vd0xp&dl=1')
@@ -20,4 +20,3 @@ pred_universe_raw.to_csv(data_directory / 'pred_universe_raw.csv', index=False)
 arrest_events_raw.to_csv(data_directory / 'arrest_events_raw.csv', index=False)
 
 
-# Save both data frames to `data/` -> 'pred_universe_raw.csv', 'arrest_events_raw.csv'
--- src/part2_preprocessing.py
+++ src/part2_preprocessing.py
@@ -5,6 +5,7 @@ PART 2: Pre-processing
 - Perform a full outer join/merge on 'person_id' into a new dataframe called `df_arrests`
 - Create a column in `df_arrests` called `y` which equals 1 if the person was arrested for a felony crime in the 365 days after their arrest date in `df_arrests`. 
 - - So if a person was arrested on 2016-09-11, you would check to see if there was a felony arrest for that person between 2016-09-12 and 2017-09-11.
+
 - - Use a print statment to print this question and its answer: What share of arrestees in the `df_arrests` table were rearrested for a felony crime in the next year?
 - Create a predictive feature for `df_arrests` that is called `current_charge_felony` which will equal one if the current arrest was for a felony charge, and 0 otherwise. 
 - - Use a print statment to print this question and its answer: What share of current charges are felonies?
@@ -23,50 +24,149 @@ from datetime import datetime, timedelta ########turns a date string into a data
 from pathlib import Path
 
 # do i need to add all this: ?????? 
-data_directory = Path('/Users/erinkeane/Desktop/414 Problem-set-2/problem-set-2/data')
+data_directory = Path('../data')
 
 def load(): 
+    '''
+    Loading data from the data directory as csv files.
+    Reads the csv as pandas dataframes. 
+    
+    Returns:
+        tuple: contains two dataframes, pred_universe_raw and arrests_events_raw. 
+         
+    '''
     pred_universe_raw = pd.read_csv(data_directory / 'pred_universe_raw.csv')
     arrest_events_raw = pd.read_csv(data_directory / 'arrest_events_raw.csv')
+    
     return pred_universe_raw, arrest_events_raw
 
-def from_etl_to_dates(pred_universe_raw, arrest_events_raw):
-    pred_universe_raw['arrest_date_univ'] = pd.to_datetime(pred_universe_raw['filing_date'])
-    arrest_events_raw['arrest_date_event'] = pd.to_datetime(arrest_events_raw['filing_date'])
-    pred_universe_raw.drop(columns=['filing_date'], inplace=True)
-    arrest_events_raw.drop(columns=['filing_date'], inplace=True)
-    return pred_universe_raw, arrest_events_raw 
+def dates(pred_universe_raw, arrest_events_raw): 
+    '''
+    This converts two columns to datetime format. 
+    
+    Parameters: 
+        pred_universe_raw (DF): contains predictive universe data.
+        arrest_events_raw (DF): contains the arrest event data.
+        
+    Returns: 
+        tuple: Contains the updated dataframes. 
 
-def merge_and_prediction(pred_universe_raw, arrest_events_raw):
+    '''
+    pred_universe_raw['arrest_date_univ'] = pd.to_datetime(pred_universe_raw['arrest_date_univ'])
+    arrest_events_raw['arrest_date_event'] = pd.to_datetime(arrest_events_raw['arrest_date_event'])
+    
+    #pred_universe_raw['arrest_date_univ'] = pd.to_datetime(pred_universe_raw['filing_date'])
+    #arrest_events_raw['arrest_date_event'] = pd.to_datetime(arrest_events_raw['filing_date'])
+    return (pred_universe_raw, arrest_events_raw)
+    
+def merge(pred_universe_raw, arrest_events_raw):
+    '''
+    Completes a full outer join on "person_id" to create df_arrests.
+    
+    Parameters: 
+        arrest_events_raw (df): contains predictive universe data.
+        arrest_events_raw (DF): contains the arrest event data.
+        
+    Returns: 
+        New dataframe: df_arrests. 
+    '''
     df_arrests = pd.merge(pred_universe_raw, arrest_events_raw, on='person_id', how='outer')
-    df_arrests['y'] = df_arrests.apply(lambda row: check_felony(row, arrest_events_raw), axis=1)
+    df_arrests.reset_index(drop=True, inplace=True)
     
+    return df_arrests
+  
+def finding_felony(arrest_events_raw, row):
+    '''
+    Going through row by row to see if a person was re-arrested for a felony within a year (365 days) after their original arrest date.
     
-    #the predictive features 
-    df_arrests['current_charge_felony'] = df_arrests['charge_type_event'].apply(lambda x: 1 if x == 'Felony' else 0)
-    df_arrests['num_fel_arrests_last_year'] = df_arrests.apply(lambda row: num_fel_arrests_last_year(row, arrests_events_raw), axis=1)
-    
-    print_statistics(df_arrests)
-    
-    #df_arrests.to_csv(data_directory / 'df_arrests.csv', index=False)
-    
-    return df_arrests 
-
-def find_felony(row, arrest_events_raw):
-    
-
-
-
-#OLD
-#def check_felony(row): 
-    #if (row['charge_type_event'] == 'Felony' and 
-        #row['arrest_date_event'] > row['arrest_date_univ'] and 
-        #row['arrest_date_event'] <= row ['arrest_date_univ'] + pd.Timedelta(days=365)):
-        #return 1 
-    #else: 
-        #return 0 
+    Parameters: 
+        arrest_events_raw (df): contains the arrest event data.
+        row (series): rows from df_arrests (the merged dataframe).
         
-          
+    Returns 
+        int: 1 if the person was re-arrested for a felony within a year after their original arrest, otherwise 0. 
+    '''
+    person_id = row["person_id"]
+    arrest_date_univ = row["arrest_date_univ"]
+    
+    first = arrest_date_univ + timedelta(days=1)
+    year_later = arrest_date_univ + timedelta(days=365)
+    
+    search_for_felony = arrest_events_raw[
+        (arrest_events_raw["person_id"] == person_id) &
+        (arrest_events_raw["arrest_date_event"] >= first) &
+        (arrest_events_raw["arrest_date_event"] <= year_later) &
+        (arrest_events_raw["charge_degree"] == "felony")
+    ]
+    
+    return 1 if not search_for_felony.empty else 0
+
+def num_fel_arrests_last_year(row, arrest_events_raw): 
+    '''
+    Counts the number of arrests in the previous year to the persons current arrest. 
+    
+    Parameters: 
+        arrest_events_raw (df): contains the arrest event data.
+        row (series): rows from df_arrests (the merged dataframe).
         
+    Returns 
+        int: The number of felony arrests in the previous year before the current charge/arrest.
+    
+    '''
+    person_id = row["person_id"]
+    arrest_date_event = row["arrest_date_event"]
+    min = arrest_date_event - timedelta(days=365)
+    max = arrest_date_event
+    
+    last_year_fel = arrest_events_raw[arrest_events_raw
+                    (arrest_events_raw["person_id"] == person_id) &
+                    (arrest_events_raw["arrest_date_event"] >= min) & 
+                    (arrest_events_raw["arrest_date_event"] < max) &
+                    (arrest_events_raw["charge_degree"] == "felony") 
+                ]
+    return last_year_fel.shape[0]
+  
+
+def add_columns_and_fix(df_arrests, arrest_events_raw):
+    '''
+    Adding the necessary columns and printing the requirements. 
+    
+    Parameters: 
+        df_arrests (DF): The merged dataframe. 
+        arrest_events_raw (Df): Contains the arrest event data.
         
-        
+    Returns: 
+        df_arrests(df): The updated dataframe with new information. 
+    
+    '''
+    df_arrests["y"] = df_arrests.apply(lambda row: finding_felony(arrest_events_raw, row), axis=1)
+    df_arrests["current_charge_felony"] = df_arrests["charge_degree"].apply(lambda x: 1 if x == 'felony' else 0)
+    df_arrests["num_fel_arrests_last_year"] = df_arrests.apply(lambda row: num_fel_arrests_last_year(row, arrest_events_raw), axis=1)
+    
+    felony = df_arrests["y"].mean() * 100 
+    current_felony = df_arrests["current_charge_felony"].mean() * 100 
+    avg_felony = df_arrests["num_fel_arrests_last_year"].mean()
+    
+    print(f" What share of arrestees in the `df_arrests` table were rearrested for a felony crime in the next year? {felony:.2f}%")
+    print(f"What share of current charges are felonies? {current_felony: .2f}%")
+    print(f"What is the average number of felony arrests in the last year? {avg_felony:.2f} ")
+    
+    return df_arrests
+
+def main(): 
+    '''
+    The main function for preprocessing. 
+    
+    '''
+    arrest_events_raw, pred_universe_raw = load()
+    arrest_events_raw, pred_universe_raw = dates(arrest_events_raw, pred_universe_raw)
+    df_arrests = merge(arrest_events_raw, pred_universe_raw)
+    df_arrests = add_columns_and_fix(df_arrests, arrest_events_raw)
+    
+    print(df_arrests.head())
+    
+    df_arrests.to_csv(data_directory / 'df_arrests.csv', index=False)
+    
+if __name__ == "__main__": 
+    main()
+    
