50963178826060c392fa11bd88f36bcec015f61d
ekeane22 <ekeane@terpmail.umd.edu>
2024-08-03 19:47:36 -0400

    quick change

--- src/part2_preprocessing.py
+++ src/part2_preprocessing.py
@@ -5,7 +5,6 @@ PART 2: Pre-processing
 - Perform a full outer join/merge on 'person_id' into a new dataframe called `df_arrests`
 - Create a column in `df_arrests` called `y` which equals 1 if the person was arrested for a felony crime in the 365 days after their arrest date in `df_arrests`. 
 - - So if a person was arrested on 2016-09-11, you would check to see if there was a felony arrest for that person between 2016-09-12 and 2017-09-11.
-
 - - Use a print statment to print this question and its answer: What share of arrestees in the `df_arrests` table were rearrested for a felony crime in the next year?
 - Create a predictive feature for `df_arrests` that is called `current_charge_felony` which will equal one if the current arrest was for a felony charge, and 0 otherwise. 
 - - Use a print statment to print this question and its answer: What share of current charges are felonies?
@@ -17,13 +16,11 @@ PART 2: Pre-processing
 - Return `df_arrests` for use in main.py for PART 3; if you can't figure this out, save as a .csv in `data/` and read into PART 3 in main.py
 '''
 
-
-# import the necessary packages
 import pandas as pd
-from datetime import datetime, timedelta ########turns a date string into a data object 
+from datetime import datetime, timedelta
 from pathlib import Path
 
-# do i need to add all this: ?????? 
+
 data_directory = Path('../data')
 
 def load(): 
